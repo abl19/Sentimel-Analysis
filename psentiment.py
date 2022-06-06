6c07fb36e6761af6ed5942251afdebcaa1437ad5
abl19 <106505515+abl19@users.noreply.github.com>
2022-06-05 23:24:32 -0400

    FTP1

--- psentiment.py
+++ psentiment.py
@@ -3,9 +3,19 @@
 Created on Sat Jun  4 17:51:31 2022
 @author: loren
 """
+from textblob import TextBlob
+import pandas as pd
+import numpy as np
+from keras import regularizers, optimizers
+from keras.layers import Embedding, Dense, Dropout, Input, LSTM, GlobalMaxPool1D
+from keras.models import Sequential
+from keras.initializers import Constant
+from tensorflow import keras
 import spacy
-# import pandas as pd 
- 
+from collections import Counter
+
+#Vectorizer = TextVectorization()
+
 # Function that will read all three files,close it and return it the store variables 
 def reading_files():
     with open(file='SW_EpisodeIV.txt',mode='r',encoding='utf-8') as file:
@@ -15,16 +25,31 @@ def reading_files():
     with open(file='SW_EpisodeVI.txt',mode='r',encoding='utf-8') as file:
         file_3=file.read()
     return file_1,file_2,file_3
+# Clean the data and obtain the Token, Lemma 
+def clean_data(file):
+    newlist=[]
+    characters=[]  # List of Characters 
+    for token in file:
+        print(token.sen)
+        #Process of removing any ing, past verb to gerund or base for  / Removing Stop Words and Puncth
+        if token.is_stop!=True and token.is_punct!=True:
+            if token.text!=" " and token.text!="\n" and  token.is_digit!=True:
+                newlist.append(tuple([token.lemma_.lower()]))  
+                if token.pos_=='PROPN':
+                    if token.text  in characters:
+                        continue
+                    else:
+                        characters.append(token.text.lower())
+    #print(characters)
+    return newlist
 nlp = spacy.load('en_core_web_lg')
 # Files 
 file_1,file_2,file_3=reading_files()
-#Step 1 -- Cleaning the data Tokenization  Process 
-#1. Obtaining Tokens 
 file_1=nlp(file_1)
-for token in file_1:
-    print(token.text,token.pos_,token.dep_)
-    #Process of removing any ing, past verb to gerund or base for
-    if not token.is_stop and not token.is_punct: #2. Removing Stop Words and Puncth
-        clean_filter=token.lemma_
-        print(token.text,token.pos_,token.dep_)
-       #print(token,clean_filter)
+# clean
+file1_clean=clean_data(file_1)
+# Obtain the most common word 
+
+
+
+
